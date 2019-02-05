a8ac32b267e01bf121b1aaee03d96f77b046510b
Manzell <manzellhatten@gmail.com>
2019-02-04 22:15:23 -0500

    Your done sir congrats

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,60 @@
+
 // An array for the Alphabet allowing an option for the Computer
 var cpuOptions = ["a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
+"b",
+"c",
+"d",
+"e",
+"f",
+"g",
+"h",
+"i",
+"j",
+"k",
+"l",
+"m",
+"n",
+"o",
+"p",
+"q",
+"r",
+"s",
+"t",
+"u",
+"v",
+"w",
+"x",
+"y",
+"z",
 ]
 
 // An array for the Alphabet allowing an option for the User
 var userOptions = ["a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
+"b",
+"c",
+"d",
+"e",
+"f",
+"g",
+"h",
+"i",
+"j",
+"k",
+"l",
+"m",
+"n",
+"o",
+"p",
+"q",
+"r",
+"s",
+"t",
+"u",
+"v",
+"w",
+"x",
+"y",
+"z",
 ]
 
 // Blank array for storing my key strokes
@@ -78,43 +79,45 @@ var numGuess = document.getElementById("numGuesses-text");
 
 document.onkeyup = function (event) {
 
-    // for (var i = 0; i <= 10; i++)
-    // Determines which key was pressed
-    var userGuess = event.key;
 
-    // Allows the computer to choose a letter in the Alphabet
-    var cpuGuess = cpuOptions[Math.floor(Math.random() * cpuOptions.length)];
+// Determines which key was pressed
+var userGuess = event.key;
 
-    for (var i = 0; i <= 10; i++){
-    // If statement that will determine which key is pressed
-    if (userGuess === userOptions) {
-        
-        
-    // The if statement continues... if the user guesses what the computer is thinking then add 1 to their score
-    }else if (cpuGuess === userGuess){
-        win++;
-    // If statement continued... if the user doesn't guess what the user is thinkning then add 1 to their losses
-    }else{
-        loss++;
-        guessesLeft--;
-    }
+// Allows the computer to choose a letter in the Alphabet
+ var cpuGuess = cpuOptions[Math.floor(Math.random() * cpuOptions.length)];
+
+
+// If statement that will determine which key is pressed
+if (userGuess === userOptions) {
+
+
+    // The if statement continues... if the user guesses what the computer is thinking then add 1 to their score and resets the array
+} else if (cpuGuess === userGuess) {
+    win++;
+    listGuess = [];
     
-}   
-    // Pushes the the key strokes from the user into an array that will display on the screen so the user knows which keys have been pressed.
-    listGuess.push(userGuess)
-  
-    // Displays our input onto the DOM with delcared variables
-    winText.textContent = "wins: " + win;
-    lossText.textContent = "losses: " + loss;
-    userInput.textContent = "Your choices: " + userGuess;
-    cpuInput.textContent = "Computer choice: " + cpuGuess;
-    numGuess.textContent = "Your guesses: " + listGuess;
+    // If statement continued... if the user doesn't guess what the user is thinkning in 10 tries then add 1 to their losses and resets the array
+} else {
+    guessesLeft--;
+    if (guessesLeft === 0) {
+        listGuess = [];
+        guessesLeft = 10;
+        loss++;
 
-    if (listGuess.length === 9) {
-        for (var i = 0; i <= 10; i++) {
-
-        }
     }
-
 }
 
+
+
+
+// Pushes the the key strokes from the user into an array that will display on the screen so the user knows which keys have been pressed.
+listGuess.push(userGuess)
+
+// Displays our input onto the DOM with delcared variables
+winText.textContent = "wins: " + win;
+lossText.textContent = "losses: " + loss;
+userInput.textContent = "Your choices: " + userGuess;
+cpuInput.textContent = "Computer choice: " + cpuGuess;
+numGuess.textContent = "Your guesses: " + listGuess;
+
+}
